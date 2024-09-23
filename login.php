077d9e97e2efe154772babf52d291e4ed82aeeac
vaibhav wakade <vaibhavwakade45@gmail.com>
2024-09-23 12:56:47 +0530

    me.php added

--- login.php
+++ login.php
@@ -1,3 +1,50 @@
+<?php
+session_start();
+
+// Database connection details
+$servername = "localhost";
+$username = "root";
+$password = "system";
+$dbname = "internship";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Check if form is submitted
+if (isset($_POST['submit'])) {
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+
+    // Fetch user from the database
+    $query = "SELECT * FROM signup WHERE email='$email'";
+    $result = mysqli_query($conn, $query);
+
+    // Check if the user exists
+    if (mysqli_num_rows($result) > 0) {
+        $user = mysqli_fetch_assoc($result);
+
+        // Verify the hashed password
+        if (password_verify($password, $user['password'])) {
+            // If the credentials are correct, redirect to software.html
+            header("Location: software.html");
+            exit();
+        } else {
+            echo "<p style='color: red;'>Invalid email or password.</p>";
+        }
+    } else {
+        echo "<p style='color: red;'>Email not registered.</p>";
+    }
+}
+
+// Close the connection
+$conn->close();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
